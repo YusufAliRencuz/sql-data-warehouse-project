23974eb7b38fbb95b783c3d4f5eb11215dd97dcb
Yusuf Ali Rencüzoğulları <yusufalirencuz@gmail.com>
2026-03-06 16:47:48 +0300

    Update 03_load_bronze.sql

--- scripts/bronze/03_load_bronze.sql
+++ scripts/bronze/03_load_bronze.sql
@@ -9,16 +9,19 @@ DESCRIPTION:
 		processes.
 ==================================================================
 PARAMETER:
-		None. 
+		@file_path: The full path of the source CSV file
 ==================================================================
 USAGE EXAMPLE:
-				EXEC bronze.load_bronze;
+		EXEC bronze.load_bronze @file_path = 'C:\YOUR_PATH\Amazon Sale Report.csv' ;
 ==================================================================
 */
 
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+CREATE OR ALTER PROCEDURE bronze.load_bronze
+	@file_path NVARCHAR(MAX) -- Take file path from outside
+AS	
 BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME -- Create variable for metadata
+	DECLARE @dynamic_sql NVARCHAR(MAX) -- Create variable for dynamic query
 	BEGIN TRY
 	SET @start_time = GETDATE();
 		-- Truncate old data of table
@@ -37,23 +40,28 @@ BEGIN
 			qty, currency, amount, ship_city, ship_state, ship_postal_code, 
 			ship_country, promotion_ids, b2b, fulfilled_by, unnamed_22
 		FROM bronze.amazon_sales_report');
-		--Insert new data into table
-		BULK INSERT bronze.vw_amazon_sales_report
-		FROM 'C:\Users\monst\OneDrive\Masaüstü\Datawarehouse_Project\Amazon Sale Report.csv'
+
+		-- Create a executable string to use parameter in bulk insert
+		SET @dynamic_sql = 'BULK INSERT bronze.vw_amazon_sales_report
+		FROM ''' + @file_path + '''
 		WITH(
-			FIELDTERMINATOR = ',',
-			ROWTERMINATOR = '0x0a',
+			FIELDTERMINATOR = '','',
+			ROWTERMINATOR = ''0x0a'',
 			FIRSTROW = 2,
-			FORMAT = 'CSV',
-			FIELDQUOTE = '"',
+			FORMAT = ''CSV'',
+			FIELDQUOTE = ''"'',
 			TABLOCK
-		);
+		);';
+
+		--Run @dynamic_sql and insert new data into table
+		EXEC sp_executesql @dynamic_sql;
+
 		SET @end_time = GETDATE();
-		PRINT '==> Data have been inserted table';
-		PRINT '==> Elapsed time: ' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) + 'second';
+		PRINT '==> Data have been inserted into table';
+		PRINT '==> Elapsed time: ' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) + ' seconds';
 	END TRY
 	BEGIN CATCH
-		PRINT '==> There is an error occur!' + ERROR_MESSAGE();
+		PRINT '==> An error occured!' + ERROR_MESSAGE();
 	END CATCH
 END
 GO

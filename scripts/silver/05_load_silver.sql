dea54d93ab7209854558012cf1e927dd348617d9
Yusuf Ali Rencüzoğulları <yusufalirencuz@gmail.com>
2026-04-07 21:32:36 +0300

    Update 05_load_silver.sql

--- scripts/silver/05_load_silver.sql
+++ scripts/silver/05_load_silver.sql
@@ -1,12 +1,9 @@
 /*
-=========================================================================
-WARNING: THIS SCRIPT ISN'T FINISHED YET
-=========================================================================
 	CREATE STORE PROCEDURE: LOAD CLEANED AND FILTERED DATA TO SILVER LAYER
 =========================================================================
 DESCRIPTION:
 			Create a procedure to load cleaned data into silver layer
-		table from bronze layer table.
+		tables from bronze layer table.
 			There are filtering, cleaning and standardization 
 		processes occurred.
 =========================================================================
@@ -26,7 +23,13 @@ BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME; -- Create variable for metadata 
 	BEGIN TRY
 		SET @start_time = GETDATE();
-		-- Truncate table
+		
+		ALTER TABLE silver.amazon_sales_promotions DROP CONSTRAINT FK_Promotions_Sales; -- Drop Foreign Key constraint from silver.amazon_sales_promotions table
+		PRINT '==> Drop constraint from silver.amazon_sales_promotions table'
+
+		-- Truncate tables
+		TRUNCATE TABLE silver.amazon_sales_promotions;
+		PRINT '==> Table is truncated: silver.amazon_sales_promotions'
 		TRUNCATE TABLE silver.amazon_sales_report;
 		PRINT '==> Table is truncated: silver.amazon_sales_report'
 
@@ -50,7 +53,10 @@ BEGIN
 		amount,
 		ship_city,
 		ship_state,
-		ship_postal_code
+		ship_postal_code,
+		ship_country,
+		b2b,
+		fulfilled_by
 		)
 		SELECT 
 		ISNULL(TRY_CAST(TRIM(index_id) AS INT), -1) AS index_id,
@@ -80,19 +86,42 @@ BEGIN
 		ISNULL(UPPER(TRIM(currency) COLLATE Latin1_General_CS_AS), 'n/a') AS currency,
 		ISNULL(TRY_CONVERT(DECIMAL(12,2), TRIM(amount)), -1) AS amount,
 		ISNULL(TRIM(LOWER(ship_city) COLLATE Latin1_General_CS_AS), 'n/a') AS ship_city,
-		ISNULL(TRIM(LOWER(ship_state) COLLATE Latin1_General_CS_AS), 'n/a') AS ship_city,
+		ISNULL(TRIM(LOWER(ship_state) COLLATE Latin1_General_CS_AS), 'n/a') AS ship_state,
 		CASE 
 			WHEN ship_postal_code NOT LIKE '%.[1-9]%' THEN CAST(CAST(ship_postal_code AS FLOAT) AS INT)
 			ELSE -1
-		END AS ship_postal_code
+		END AS ship_postal_code,
+		ISNULL(TRIM(ship_country), 'n/a'),
+		ISNULL(TRIM(b2b), 'n/a'),
+		ISNULL(TRIM(fulfilled_by), 'n/a')
 		FROM(
 		SELECT 
 		*,
 		ROW_NUMBER() OVER (PARTITION BY order_id, sku, style, asin, category, size, qty, amount ORDER BY TRY_CONVERT(DATE ,order_date, 10) DESC ) AS duplicate_row
 		FROM bronze.amazon_sales_report
 		)t WHERE duplicate_row = 1
-		SET @end_time = GETDATE();
-		PRINT '==> Data have been inserted into table';
+
+		-- Insert clean data into silver.amazon_sales_promotions table
+		INSERT INTO silver.amazon_sales_promotions(
+		sale_id,
+		promotion_id 
+		)
+		SELECT 
+		s.sale_id, 
+		TRIM(promo.value)
+		FROM silver.amazon_sales_report AS s
+		INNER JOIN bronze.amazon_sales_report as b
+			ON s.order_id = b.order_id
+		CROSS APPLY STRING_SPLIT(b.promotion_ids, ',') AS promo
+		WHERE b.promotion_ids IS NOT NULL;
+		PRINT '==> Data have been inserted into tables';
+
+		-- Re-add constraint to silver.amazon_sales_promotions table
+		ALTER TABLE silver.amazon_sales_promotions
+		ADD CONSTRAINT FK_Promotions_Sales FOREIGN KEY (sale_id) REFERENCES silver.amazon_sales_report (sale_id);
+		PRINT '==> Re-add constraint to silver.amazon_sales_promotions table'
+
+		SET @end_time = GETDATE()
 		PRINT '==> Elapsed time:' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 	END TRY
 	BEGIN CATCH
